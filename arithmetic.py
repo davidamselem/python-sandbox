4e7b6b247e118b8b468b1a3aa11bcb06075eb0ba
David Amselem, B. Eng <github@amselem.co.il>
2019-05-15 01:14:51 +0300

    Core et. al.

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+
+
+
+# Python 3: Simple arithmetic
+#
+
+print(1 / 2)
+
+print(2 ** 3)
+
+# classic division returns a float
+print(17 / 3)
+
+# floor division
+print(17 // 3)
---
+++ comprehension.py
@@ -0,0 +1,16 @@
+
+
+
+# Python 3: List comprehensions
+#
+
+fruits = ['Banana', 'Apple', 'Lime']
+
+loud_fruits = [fruit.upper() for fruit in fruits]
+
+print(loud_fruits)
+
+# List and the enumerate function
+#
+
+list(enumerate(fruits))
